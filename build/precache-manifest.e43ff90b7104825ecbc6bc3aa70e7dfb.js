2f8ad7f0f38915652f308405f5ce8a81281c5e5a
Alex <klealekle@gmail.com>
2020-05-07 12:26:49 +0300

    bild add

---
+++ build/precache-manifest.e43ff90b7104825ecbc6bc3aa70e7dfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209e6d540920824db23e78668f8ffb00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8b8bd0fd52cc51081b5",
+    "url": "/static/css/2.7a75a24e.chunk.css"
+  },
+  {
+    "revision": "1af6765c5db7bb2f2361",
+    "url": "/static/css/main.7788c5cc.chunk.css"
+  },
+  {
+    "revision": "a8b8bd0fd52cc51081b5",
+    "url": "/static/js/2.58f9631c.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/static/js/2.58f9631c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af6765c5db7bb2f2361",
+    "url": "/static/js/main.223ecdda.chunk.js"
+  },
+  {
+    "revision": "c63617fb22805bf2a9a7",
+    "url": "/static/js/runtime-main.f848c31e.js"
+  },
+  {
+    "revision": "98279f8f8463aabd151af0d4951ca41a",
+    "url": "/static/media/preload.98279f8f.gif"
+  }
+]);
