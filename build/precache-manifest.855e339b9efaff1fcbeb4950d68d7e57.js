1e13fc3603b8b872a42a34a73a745e53580629ea
Alex <klealekle@gmail.com>
2020-07-02 11:03:58 +0300

    organizationHeader text

---
+++ build/precache-manifest.855e339b9efaff1fcbeb4950d68d7e57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38953e169062f1f74e9c60f0878c40fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f65312dd70c7a4105049",
+    "url": "/static/css/2.7a75a24e.chunk.css"
+  },
+  {
+    "revision": "6e82adf7873cfc084af6",
+    "url": "/static/css/main.95405c9f.chunk.css"
+  },
+  {
+    "revision": "f65312dd70c7a4105049",
+    "url": "/static/js/2.cdcd5364.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/static/js/2.cdcd5364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e82adf7873cfc084af6",
+    "url": "/static/js/main.a0100f90.chunk.js"
+  },
+  {
+    "revision": "c63617fb22805bf2a9a7",
+    "url": "/static/js/runtime-main.f848c31e.js"
+  },
+  {
+    "revision": "98279f8f8463aabd151af0d4951ca41a",
+    "url": "/static/media/preload.98279f8f.gif"
+  }
+]);
