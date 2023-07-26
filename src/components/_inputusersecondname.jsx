4729e400c7764030e3d687453a9ddd5d53ca36da
Kirill <kirisakov4@yandex.ru>
2023-07-26 17:34:33 +0300

    Исправление Валидации форм
    Исправлена валидация для форм
    usename;
    usersecondname;
    usershurname.
    Исправлен текст ошибки при валидации.
    Удалена страна "Российская Федерация при выборе "Нерезидента"

--- src/components/_inputusersecondname.jsx
+++ src/components/_inputusersecondname.jsx
@@ -1,68 +1,69 @@
-import React from "react";
-import InputComponent from "./inputcomponent";
+import React from 'react'
+import InputComponent from './inputcomponent'
 
 class InputUserSecondName extends InputComponent {
-  handleChange = e => {
+  handleChange = (e) => {
     // fire the form change event
-    this.props.onChange(e);
-    this.validateValue(e.target);
-  };
-
-  validateValue(input) {
-    let result = false;
-    const minLen = this.props.minLen;
-    const value = input.value;
-
-    if (value && this.validateSymbols(input) && value.length > minLen ) {
-      result = true;
-      this.setValid(input);
-    } else {
-      this.setInvalid(input);
-    }
-    this.props.onValidate(result);
+    this.props.onChange(e)
+    this.validateValue(e.target)
   }
 
-  validateSymbols(input){
-    const reSpace = /^((?!\s{1}).)*$/;
-    const txt = input.value;
-    let result = false;
+  validateValue(input) {
+    let result = false
+    const minLen = this.props.minLen
+    const value = input.value
+
+    if (value && this.validateSymbols(input) && value.length > minLen) {
+      result = true
+      this.setValid(input)
+    } else {
+      this.setInvalid(input)
+    }
+    this.props.onValidate(result)
+  }
+
+  validateSymbols(input) {
+    const reSpace = /^((?!\s{1}).)*$/
+    const txt = input.value
+    let result = false
     if (reSpace.test(String(txt))) {
       //const re = /^[?!,'":@*—+«‎»()\\/\-_.а-яА-ЯёЁ0-9a-zA-Z\s]+$/;
-      const re = /^[?!,'"—«‎»\-_.а-яА-ЯёЁ0-9a-zA-Z\s]+$/;
+      // const re = /^[?!,'"—«‎»\-_.а-яА-ЯёЁ0-9a-zA-Z\s]+$/;
+      const re = /^[а-яА-ЯёЁa-zA-Z]+$/
       if (re.test(String(txt))) {
-        result = true;
+        result = true
       } else {
-        result = false;
+        result = false
       }
-      console.log("validateInput:" + result);
+      console.log('validateInput:' + result)
     } else {
-      result = false;
+      result = false
     }
-    return result;
+    return result
   }
 
   render() {
     return (
-      <div className={this.props.shouldHide ? 'mb-3 hidden' : 'mb-3'} >
+      <div className={this.props.shouldHide ? 'mb-3 hidden' : 'mb-3'}>
         {/* <label htmlFor="userSecondName">{this.props.label}</label> */}
-        <div className="floating-label-group">
+        <div className='floating-label-group'>
           <input
-            type="text"
-            className="form-control"
-            id="userSecondName"
-            name="userSecondName"
+            type='text'
+            className='form-control'
+            id='userSecondName'
+            name='userSecondName'
             onChange={this.handleChange}
             value={this.props.value}
             required
           />
-          <span className="floating-label">{this.props.label}</span>
-          <div className="invalid-feedback" stylename="width: 100%">
+          <span className='floating-label'>{this.props.label}</span>
+          <div className='invalid-feedback' stylename='width: 100%'>
             {this.props.invalidMessage}
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default InputUserSecondName;
+export default InputUserSecondName
--- src/components/_inputusershurname.jsx
+++ src/components/_inputusershurname.jsx
@@ -1,69 +1,70 @@
-import React from "react";
-import InputComponent from "./inputcomponent";
+import React from 'react'
+import InputComponent from './inputcomponent'
 
 class InputUserSurname extends InputComponent {
-  handleChange = e => {
+  handleChange = (e) => {
     // fire the form change event
-    this.props.onChange(e);
-    this.validateValue(e.target);
-  };
-
-  validateValue(input) {
-    let result = false;
-    const minLen = this.props.minLen;
-    const value = input.value;
-
-    if (value && this.validateSymbols(input) && value.length > minLen ) {
-      result = true;
-      this.setValid(input);
-    } else {
-      this.setInvalid(input);
-    }
-    this.props.onValidate(result);
+    this.props.onChange(e)
+    this.validateValue(e.target)
   }
 
-  validateSymbols(input){
-    const reSpace = /^((?!\s{1}).)*$/;
-    const txt = input.value;
-    let result = false;
+  validateValue(input) {
+    let result = false
+    const minLen = this.props.minLen
+    const value = input.value
+
+    if (value && this.validateSymbols(input) && value.length > minLen) {
+      result = true
+      this.setValid(input)
+    } else {
+      this.setInvalid(input)
+    }
+    this.props.onValidate(result)
+  }
+
+  validateSymbols(input) {
+    const reSpace = /^((?!\s{1}).)*$/
+    const txt = input.value
+    let result = false
     if (reSpace.test(String(txt))) {
       //const re = /^[?!,'":@*—+«‎»()\\/\-_.а-яА-ЯёЁ0-9a-zA-Z\s]+$/;
-      const re = /^[?!,'"—«‎»\-_.а-яА-ЯёЁ0-9a-zA-Z\s]+$/;
+      // const re = /^[?!,'"—«‎»\-_.а-яА-ЯёЁ0-9a-zA-Z\s]+$/;
+      const re = /^[а-яА-ЯёЁa-zA-Z]+$/
       if (re.test(String(txt))) {
-        result = true;
+        result = true
       } else {
-        result = false;
+        result = false
       }
-      console.log("validateInput:" + result);
+      console.log('validateInput:' + result)
     } else {
-      result = false;
+      result = false
     }
-    return result;
+    return result
   }
 
   render() {
     return (
-      <div className={this.props.shouldHide ? 'mb-3 hidden' : 'mb-3'} >
+      <div className={this.props.shouldHide ? 'mb-3 hidden' : 'mb-3'}>
         {/* <label htmlFor="userSurname">{this.props.label}</label> */}
-        <div className="floating-label-group">
+        <div className='floating-label-group'>
           <input
-            type="text"
-            className="form-control"
-            id="userSurname"
-            name="userSurname"
+            type='text'
+            className='form-control'
+            id='userSurname'
+            name='userSurname'
             onChange={this.handleChange}
             value={this.props.value}
             required
           />
-          
-          <span className="floating-label">{this.props.label}</span>
-          <div className="invalid-feedback" stylename="width: 100%">
+
+          <span className='floating-label'>{this.props.label}</span>
+          <div className='invalid-feedback' stylename='width: 100%'>
             {this.props.invalidMessage}
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default InputUserSurname;
+export default InputUserSurname
